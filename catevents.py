305e1dfd15cc808eac3f322bd416234f6fa67b48
Steven Sherburn <s.sherburn@gns.cri.nz>
2016-09-17 16:02:20 +1200

    polygon search and no longer use bar for magnitudes

--- catevents.py
+++ catevents.py
@@ -3,23 +3,32 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-url = 'http://wfs.geonet.org.nz/geonet/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geonet:quake_search_v1&maxFeatures=2000&outputFormat=csv'
-cat = pd.read_csv(url, parse_dates=[2])
+url = "http://wfs.geonet.org.nz/geonet/ows?service=WFS&version=1.0.0&request=GetFeature&typeName=geonet:quake_search_v1&outputFormat=csv&cql_filter=origintime>='2011-01-01T00:00:00.000Z'+AND+depth<50+AND+WITHIN(origin_geom,POLYGON((175.5957+-39.1695,+175.6478+-39.0827,+175.7036+-39.1121,+175.6619+-39.1833,+175.5957+-39.1695)))+AND+depth<20"
+cat = pd.read_csv(url, parse_dates=['origintime'])
 
 #sort by origintime, so events in time order
 cat.sort_values(['origintime'], ascending=True, inplace=True)
 cat = cat.reset_index()
 
-fig = plt.figure(figsize=(15, 5))
+fig = plt.figure(figsize=(15, 15))
 
 #magnitude vs time
-ax1 = fig.add_subplot(2, 1, 1)
-time = pd.to_datetime(cat.origintime)
-ax1.bar(time, cat.magnitude, width = 0.01, color='red', edgecolor='red', align='edge')
+ax1 = fig.add_subplot(3, 1, 1)
+#automatic locations
+time = pd.to_datetime(cat.origintime[cat['evaluationmode']=='automatic'])
+automatic = cat.magnitude[cat['evaluationmode']=='automatic']
+ax1.plot(time, automatic, marker='o', color='red', linestyle='None', label='automatic')
+#ax1.bar(time, automatic, width = 0.005, color='red', edgecolor='red', align='edge', label='automatic')
+#manual locations
+time = pd.to_datetime(cat.origintime[cat['evaluationmode']=='manual'])
+manual = cat.magnitude[cat['evaluationmode']=='manual']
+ax1.plot(time, manual, marker='o', color='blue', linestyle='None', label='manual')
+#ax1.bar(time, manual, width = 0.005, color='blue', edgecolor='blue', align='edge', label='manual')
 plt.ylabel('magnitude')
+plt.legend()
 
 #cumulative number
-ax2 = fig.add_subplot(2, 1, 2, sharex = ax1)
+ax2 = fig.add_subplot(3, 1, 2, sharex = ax1)
 ax2.plot(cat.origintime, cat.index, color='red', marker='None', label='cumnum')
 ax2.tick_params(axis='y', colors='red')
 ax2.set_ylabel('cumulative number', color = 'red')
@@ -32,3 +41,17 @@ cat['cumeng'] = cat['cumeng'] / cat['cumeng'].max()
 ax2a.plot(cat.origintime, cat.cumeng, color='blue', marker='None', label='cumeng')
 ax2a.tick_params(axis='y', colors='blue')
 ax2a.set_ylabel('cumulative energy', color = 'blue')
+
+#depth
+ax3 = fig.add_subplot(3, 1, 3, sharex = ax1)
+#automatic locations
+time = pd.to_datetime(cat.origintime[cat['evaluationmode']=='automatic'])
+automatic = cat.depth[cat['evaluationmode']=='automatic']
+ax3.plot(time, automatic, color='red', marker='o', linestyle='None', label='automatic')
+#manual locations
+time = pd.to_datetime(cat.origintime[cat['evaluationmode']=='manual'])
+manual = cat.depth[cat['evaluationmode']=='manual']
+ax3.plot(time, manual, color='blue', marker='o', linestyle='None', label='manual')
+plt.gca().invert_yaxis()
+ax3.set_ylabel('depth(km)')
+plt.legend()
